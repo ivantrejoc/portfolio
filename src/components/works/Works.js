d5b87f625e61fc51fb43364238ee1c05bc981d2d
ivantrejoc <IVANJTREJOC@GMAIL.COM>
2023-09-14 09:01:32 -0400

    links done

--- src/components/works/Works.js
+++ src/components/works/Works.js
@@ -5,17 +5,17 @@ import { Container } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { TextDecrypt } from "../content/TextDecrypt";
 
-import './Works.css';
+import "./Works.css";
 
 // Import ../../assets/recentprojects/
-import Wanderlust from '../../assets/recentprojects/wanderlust.png';
-import Pokemon from '../../assets/recentprojects/pokemon.png';
+import Wanderlust from "../../assets/recentprojects/wanderlust.png";
+import Pokemon from "../../assets/recentprojects/pokemon.png";
 import Portfolio from "../../assets/recentprojects/portfolio.png";
 
 const useStyles = makeStyles((theme) => ({
   main: {
-    maxWidth: '100vw',
-    marginTop: '3em',
+    maxWidth: "100vw",
+    marginTop: "3em",
     marginBottom: "auto",
   },
 }));
@@ -23,56 +23,51 @@ const useStyles = makeStyles((theme) => ({
 export const Works = () => {
   const classes = useStyles();
   const [projects, setProjects] = useState([
-    { 
+    {
       id: 1,
-      title: 'React Portfolio', 
+      title: "React Portfolio",
       description: `Personal Portfolio developed with ReactJS.  
       Designed with fancy 3D animations using Three.js for 
       the background element and MUI components.`,
-      alter: 'React Portfolio',
+      alter: "React Portfolio",
       image: `${Portfolio}`,
-      url: "https://ivantrejo.vercel.app/"
+      url: "https://ivantrejo.vercel.app/",
     },
-    { 
+    {
       id: 2,
-      title: 'Wanderlust', 
+      title: "Wanderlust",
       description: `Amazing e-commerce aimed to sell travel packages. Stands out for its third-party authentication, DBB, sorts, filters, shopping cart and PayPal payment gateway.`,
-      alter: 'Wanderlust',
+      alter: "Wanderlust",
       image: `${Wanderlust}`,
-      url:"https://wanderlust-phi.vercel.app/home"
+      url: "https://wanderlust-phi.vercel.app/home",
     },
-    { 
+    {
       id: 3,
-      title: 'Pokemon', 
+      title: "Pokemon",
       description: `ReactJS app developed to render pokemon API characters. Front-end designed with tailwindCSS and Flowbite components, back-end developed with Express and database developed with Sequelize.`,
-      alter: 'Pokemon',
+      alter: "Pokemon",
       image: `${Pokemon}`,
-      url: "https://pokemonivan.vercel.app/"
+      url: "https://pokemonivan.vercel.app/",
     },
-   
   ]);
 
   return (
     <section id="works">
       <Container component="main" className={classes.main} maxWidth="md">
         {projects.map((project) => (
-          
-          <div className="project" key={ project.id }>
-            <a href={project.url} className="link"> 
+          <div className="project" key={project.id}>
             <div className="__img_wrapper">
-              <img src={ project.image } alt={ project.alter }/>
+              <img src={project.image} alt={project.alter} />
             </div>
-            </a>
+
             <div className="__content_wrapper">
-              <a href={project.url} className="link"> 
-              <h3 className="title">
-                <TextDecrypt text={ project.id + '. ' + project.title } />
-              </h3>
+              <a href={project.url} className="link">
+                <h3 className="title">
+                  <TextDecrypt text={project.id + ". " + project.title} />
+                </h3>
               </a>
-              
-              <p className="description">
-                { project.description }
-              </p>
+
+              <p className="description">{project.description}</p>
             </div>
           </div>
         ))}
